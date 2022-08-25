211cb6009c2084879afa253903d293488e99f601
kwilkins <kwilkins@mail.com>
2022-08-25 14:01:42 -0500

    Created index.php file to eventually replace index.html.

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="stylesheets/global.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/navigation.css">
+
+    <title>Kevin Wilkins</title>
+</head>
+
+<body>
+    
+    <!--Navigation-->
+    <nav>
+        <p style="font-family: 'Courier New', Courier, monospace; size: 18px; cursor: default;">
+            Kevin Wilkins
+        </p>
+        <ul>
+            <li><a class="active" href="index.html">Home</a></li>
+        </ul>
+
+        <div class="SiteVersion">
+            <i><font color=white>Version 0.2.1</font></i>
+        </div>
+    </nav>
+
+    <!--Content-->
+    <div class="content">
+        <h1 style="font-family: 'Courier New', Courier, monospace;">
+            Hello!
+        </h1>
+    </div>
+
+    <div class="grid-container">
+        <div>
+            <img style="float: left; margin-left: 10px; margin-right: 10px; margin-bottom: 10px;" src="images/kwilkins.jpg" height="165px" width="200px">
+
+        <p class="paragraph">
+            Email:  <a href="mailto:kwilkins@mail.com" style="font-family: monospace;">kwilkins@mail.com</a>
+        </p>
+        </div>
+
+        <div style="max-width: 80%;">
+            <h2 style="font-family: Arial, Helvetica, sans-serif;">
+                Welcome to my website.
+            </h2>
+    
+            <p class="paragraph">
+                I work for an Internet Service Provider (ISP) providing end user support for data, internetwork, and voice services.  In my off time I enjoy tinkering with my home network and coding.
+            </p>
+                
+            <p class="paragraph">
+                I have a few projects I am working on that can be found on my <a href="https://github.com/kawilkins" target="_blank">GitHub</a>.  These projects mainly include work in HTML/CSS, JavaScript, and C++.  I have also developed a couple of Discord Bots for a <a href="https://discord.gg/w4T7CSc">Discord server</a> written in Python.
+            </p>
+            
+            <p class="paragraph">
+                While the extent of my work is smaller than I would like I am proactive in challenging myself.  My future goals include developing a contact form for this website using PHP and JavaScript.
+            </p>
+
+            <p class="paragraph">
+                If you like what you see here you can encourage my efforts by <i>buying me a coffee</i>.  Any donations accepted will be used for purchasing necessary equipment and supporting local businesses in my area.  This also helps support my <i>side gig</i> of helping family and friends with their IT requests.
+            </p>
+            
+            <br></br>
+
+            <div class="BuyMeCoffee">
+            <!--Buy Me A Coffee Button-->
+                <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="kwilkins" data-color="#5F7FFF" data-emoji=""  data-font="Lato" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
