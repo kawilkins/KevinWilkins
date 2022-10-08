29049df0f9899667d3b4c1e949542b25663ebe58
kwilkins <kwilkins@mail.com>
2022-10-08 00:04:47 -0500

    Add global JavaScript file. Add email address. Add website version.

---
+++ javascript/global.js
@@ -0,0 +1,7 @@
+// Website version
+let siteVersion = '0.3.1';
+document.getElementById('revision').innerHTML = "v" + siteVersion;
+
+// Email
+let email = "kwilkinsrd@gmail.com";
+document.getElementById('Email').innerHTML = email;
