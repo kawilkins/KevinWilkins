a6312c960778bb359f666ea44dd3f82f066fbcc3
kwilkins <kwilkins@mail.com>
2022-11-10 19:35:42 -0600

    Changed email and siteVersion from 'var' to 'const'.

--- javascript/global.js
+++ javascript/global.js
@@ -1,7 +1,7 @@
 // Website version
-let siteVersion = '0.3.1';
+const siteVersion = '0.3.2';
 document.getElementById('revision').innerHTML = "v" + siteVersion;
 
 // Email
-let email = "kwilkinsrd@gmail.com";
+const email = "kwilkinsrd@gmail.com";
 document.getElementById('Email').innerHTML = email;
