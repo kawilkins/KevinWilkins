de062732927ad4f6ccfdba07882b858bef4c887a
kwilkins <kwilkins@mail.com>
2022-11-10 21:01:05 -0600

    Add function for responsive navigation.

--- javascript/global.js
+++ javascript/global.js
@@ -5,3 +5,12 @@ document.getElementById('revision').innerHTML = "v" + siteVersion;
 // Email
 const email = "kwilkinsrd@gmail.com";
 document.getElementById('Email').innerHTML = email;
+
+// Site navigation
+function responsiveNavigation() {
+    let x = document.getElementById("siteNavigation");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    }
+    else {x.className = "nav";}
+}
