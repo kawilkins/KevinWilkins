c9a360352e72e4869beca3fe61a3e971918e0fca
kwilkins <kwilkinsrd@gmail.com>
2023-12-22 22:35:06 -0600

    Updated website revision from 1.0.1 => 1.1.0.

--- javascript/global.js
+++ javascript/global.js
@@ -3,6 +3,6 @@ const email = "kwilkinsrd@gmail.com";
 document.getElementById('Email').innerHTML = email;
 
 // Site Version
-const revision = "1.0.1";
+const revision = "1.1.0";
 siteVersion = "v" + revision;
 document.getElementById('CurrentVersion').innerHTML = siteVersion;
