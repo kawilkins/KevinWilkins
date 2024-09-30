0bd056fd68876c68163e5b8229117e9bd7eba88b
Kevin Wilkins <kwilkinsrd@gmail.com>
2024-09-30 14:07:16 -0500

    fix: Add bug report to navigation
    Added bug report to navigation.

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -3,12 +3,10 @@ class TopNavigation extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
         <nav>
-            <h1>
-                Kevin Wilkins
-            </h1>
             <ul>
                 <li><a href="index.html">Home</a></li>
                 <li><a href="pwgen.html">Password Generator</a></li>
+                <li><a href="https://github.com/kawilkins/KevinWilkins/issues" target="_blank">Bug Report</a></li>
             </ul>
         </nav>`
     }
