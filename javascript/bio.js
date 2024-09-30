8b3bc2c5e49ace2c7248c2cb39551558d4371476
Kevin Wilkins <kwilkinsrd@gmail.com>
2024-09-30 14:04:27 -0500

    feat: Bio overview
    Added brief bio overview in the form of a bullet point style similar to
    a resume/CV.

--- javascript/bio.js
+++ javascript/bio.js
@@ -1,9 +1,17 @@
 // Brief intro
-class brief_bio extends HTMLElement {
+class briefBio extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
-        `
+        <b>Current role:</b> MIS Administrator</br>
+        <b>Projects and Responsibilities:</b>
+            <ul>
+                <li>Migrated employee emails to Microsoft Exchange</li>
+                <li>Responded to and resolve employee trouble tickets</li>
+                <li>Migrated servers from ceph system to VMware solution</li>
+                <li>Participated in on-call rotation</li>
+            </ul>
+        <b>Time with employer:</b> 5 years `
     }
 }
 
-customElements.define('bio', brief_bio)
+customElements.define('brief-bio', briefBio)
