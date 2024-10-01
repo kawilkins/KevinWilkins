f9e547293e1ba1fc5a0089e50aab422f220dddc4
Kevin Wilkins <kwilkinsrd@gmail.com>
2024-10-01 12:14:43 -0500

    fix: Update site version.
    Updated site version.

--- javascript/global.js
+++ javascript/global.js
@@ -3,6 +3,6 @@ const email = "kwilkinsrd@gmail.com";
 document.getElementById('Email').innerHTML = email;
 
 // Site Version
-const revision = "2.0.0";
+const revision = "2.1.1";
 siteVersion = "v" + revision;
 document.getElementById('CurrentVersion').innerHTML = siteVersion;
