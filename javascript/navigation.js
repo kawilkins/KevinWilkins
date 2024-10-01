13f4087c1b93ca6af43a42166a84b8a7b45cfcd2
Kevin Wilkins <kwilkinsrd@gmail.com>
2024-10-01 12:15:15 -0500

    fix: Add contact to navigation
    Added contact.html to top navigation.

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -5,6 +5,7 @@ class TopNavigation extends HTMLElement {
         <nav>
             <ul>
                 <li><a href="index.html">Home</a></li>
+                <li><a href="contact.html">Contact</a></li>
                 <li><a href="pwgen.html">Password Generator</a></li>
                 <li><a href="https://github.com/kawilkins/KevinWilkins/issues" target="_blank">Bug Report</a></li>
             </ul>
