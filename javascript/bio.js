997a91fa52d59d5b2f3c0ea010652a70c09e0fec
Kevin Wilkins <kwilkinsrd@gmail.com>
2024-12-16 12:55:17 -0600

    feat: Convert ms to time
    Added conversion for time units.

--- javascript/bio.js
+++ javascript/bio.js
@@ -6,6 +6,19 @@ class briefBio extends HTMLElement {
         const diffDate = now - startDate;
         const timeSince = new Date(diffDate);
 
+        const convertYear = 365.25 * 24 * 3600 * 1000;
+        const convertMonth = convertYear / 12;
+        const convertWeek = 7 * 24 * 3600 * 1000;
+        const convertDay = 24 * 3600 * 1000;
+
+        const year = Math.floor(diffDate / convertYear);
+        const remainderYear = diffDate % convertYear;
+        const month = Math.floor(remainderYear / convertMonth);
+        const remainderMonth = remainderYear % convertMonth;
+        const week = Math.floor(remainderMonth / convertWeek);
+        const remainderWeek = remainderMonth % convertWeek;
+        const day = Math.floor(remainderWeek / convertDay);
+
         console.log("Start Date:", startDate);
         console.log("Current Date:", now);
         console.log("Years:", year, "Months:", month, "Weeks:", week, "Days:", day);
